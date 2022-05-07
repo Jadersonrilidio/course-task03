6b07f7ca9665747d6417ffe34b2399599bbc2855
jay <jadersonrilidio@gmail.com>
2022-05-07 12:31:29 -0300

    js script refactored

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
-
 class ValidateForm {
 
+    /**
+     * Class constructor
+     * 
+     */
     constructor() {
         this.formulario = document.querySelector('#form');
         this.handleEvent();
     }
 
+    /**
+     * Acts over page form events
+     * 
+     */
     handleEvent() {
         this.formulario.addEventListener('submit', event => {
             this.handleSubmit(event);
         });
     }
 
+    /**
+     * Handles the form submit event
+     * 
+     * @param {*} event
+     * 
+     */
     handleSubmit(event) {
         event.preventDefault();
-
         const validFields = this.hasEmptyFields();
-
-        if (validFields === true) {
-            this.triggerSuccess('Form submitted with success!');
-        } else {
-            this.triggerError('All fields must be filled');
-        }
+        (validFields === true) 
+            ? this.triggerMessage('Form submitted with success!', 'success-msg') 
+            : this.triggerMessage('All fields must be filled', 'error-msg');
     }
 
+    /**
+     * Checks whether the required are filled or not
+     * 
+     * @returns boolean
+     * 
+     */
     hasEmptyFields() {
-        let valid = true;
-
         for (let field of this.formulario.querySelectorAll('.validate'))
             if (!field.value)
-                valid = false;
-
-        return valid;
+                return false;
+        return true;
     }
 
-    triggerError(msg) {
+    /**
+     * Display the status message under triggered events
+     * 
+     * @param {*} msg 
+     * @param {*} className
+     *  
+     */
+    triggerMessage(msg, className) {
         const span = document.querySelector('.status-msg');
-        span.classList.add('error-msg');
-        span.innerHTML = msg;
-    }
-
-    triggerSuccess(msg) {
-        const span = document.querySelector('.status-msg');
-        span.classList.add('success-msg');
+        span.classList.add(className);
         span.innerHTML = msg;
     }
 }
